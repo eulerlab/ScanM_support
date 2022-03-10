f3d5f4659c1bbf4243b437256480ce78c2995e40
jonathanoesterle <jonathan.oesterle1@gmail.com>
2022-03-10 14:42:56 +0100

    Fixed imports

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ScanM_support',
+    version='',
+    packages=['scanmsupport', 'scanmsupport.scanm'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description='ScanM'
+)
